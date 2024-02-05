3ee17b70ee07e0258bcfefa7e558c8a7036b20cf
Giorgi Koguashvili <kogo27@gmail.com>
2024-02-05 20:31:00 +0400

    Update pureChurchFactorial.js

--- src/pureChurchFactorial.js
+++ src/pureChurchFactorial.js
@@ -1,3 +1,9 @@
+/*
+const pureChurchEncodedFactorialFunctionExecutedWithNumber7 = (f => (x => f(v => x(x)(v)))(x => f(v => x(x)(v))))(f => n => (b => A => B => b(A)(B)())((n => n(x => _ => f => f)(t => _ => t))(n))(_ => f => x => f(x))(_ => (n0 => n1 => n1((n0 => n1 => f => x => n1(f)(n0(f)(x)))(n0))(f => x => x))(n)(f((n => (p => p(t => _ => t) )( n  (p => (x1 => x2 => f => f(x1)(x2))( (p => p(_ => f => f))(p))( (n => f => x => f(n(f)(x)))((p => p(_ => f => f))(p)))) ((x1 => x2 => f => f(x1)(x2))(f => x => x)(f => x => x))))(n)))))(f => x => f(f(f(f(f(f(f(x))))))))
+
+(n => n(i => i + 1)(0))(pureChurchEncodedFactorialFunctionExecutedWithNumber7); // 5040
+*/
+
 // Pure Church Encoded Factorial Recurtion Function
 const churchEncodedFactorialFunction = (f => (x => f(v => x(x)(v)))(x => f(v => x(x)(v))))(f => n => (b => A => B => b(A)(B)())((n => n(x => _ => f => f)(t => _ => t))(n))(_ => f => x => f(x))(_ => (n0 => n1 => n1((n0 => n1 => f => x => n1(f)(n0(f)(x)))(n0))(f => x => x))(n)(f((n => (p => p(t => _ => t) )( n	(p => (x1 => x2 => f => f(x1)(x2))( (p => p(_ => f => f))(p))( (n => f => x => f(n(f)(x)))((p => p(_ => f => f))(p)))) ((x1 => x2 => f => f(x1)(x2))(f => x => x)(f => x => x))))(n)))))
 
